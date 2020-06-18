9becc93d8fdcc75699653a5f485f296a9c6b3416
HafsaR <hafsa34343@gmail.com>
2020-06-18 11:04:13 -0600

    more challenges

---
+++ substring/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a function that checks if a string contains the substring "abc".
+            Input: string
+            Output: bool
+            */
+
+            Console.WriteLine("Enter string: ");
+            string inputString = Console.ReadLine();
+           
+            bool result = checkSubString(inputString);
+            Console.WriteLine(result);
+
+        }
+        static bool checkSubString(string str){
+             string substr="abc";
+            if(str.Contains(substr)){
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+    }
+}
---
+++ value-checker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace value_checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a function that checks if a number is both greater than 0 AND a multiple of 3.
+                    Input: int
+                    Output: bool
+            */
+
+            Console.WriteLine("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool result = valueChecker(number);
+            
+            Console.WriteLine($"Result is {result}");
+        }
+        static bool valueChecker(int number){
+            if(number>0 && number%3 == 0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+}
