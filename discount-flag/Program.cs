2dea4a66a65a026a4a2e189fad798c40f4d86098
HafsaR <hafsa34343@gmail.com>
2020-06-18 16:43:05 -0600

    discount

---
+++ discount-flag/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace discount_flag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                /*
+        9. Write a function that takes in the price of an item and a "discount" flag.
+        Apply a 15% tax to the price. If the discount flag is toggled, then take $2 off the price.
+        */
+
+        Console.WriteLine("Enter price: ");
+        double itemCost = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter flag");
+        string flag = Console.ReadLine();
+        Console.WriteLine($"Item cost is ${itemCost}");
+        double totalCost = discount_flag(itemCost,flag);
+        Console.WriteLine($"Total cost of item is {totalCost}");
+
+        }
+        static double discount_flag(double cost,string flag){
+            if(flag.Equals("discount")){
+                double salesTax=15;
+                double tax = salesTax/100;
+            
+                double totalSalesTax = cost * tax;
+            
+                double total = cost + totalSalesTax;
+               
+                return total;
+
+            }else if(flag.Equals("toggled")){
+                double total = cost - 2;
+                Console.WriteLine("total: "+total);
+                return total;
+            }else{
+                return cost;
+            }
+
+           
+        }
+    }
+}
---
+++ leap-year/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace leap_year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*8. Write a function to check if a given year is a leap year. You may need to look up the rules for what counts as a leap year.
+           
+           How to determine whether a year is a leap year
+            To determine whether a year is a leap year, follow these steps:
+
+                if year is divisible by 400 then is_leap_year
+                else if year is divisible by 100 then not_leap_year
+                else if year is divisible by 4 then is_leap_year
+                else not_leap_year
+           */
+           Console.WriteLine("Enter year: ");
+           int year = Convert.ToInt32(Console.ReadLine());
+           bool result = leap_year(year);
+          Console.WriteLine(result?"Leap year": "Not a leap year");
+           
+        }
+        static bool leap_year(int year){
+        // If a year is multiple of 400, 
+		// then it is a leap year 
+		if (year % 400 == 0) 
+			return true; 
+	
+		// Else If a year is muliplt of 100, 
+		// then it is not a leap year 
+		if (year % 100 == 0) 
+			return false; 
+	
+		// Else If a year is muliplt of 4, 
+		// then it is a leap year 
+		if (year % 4 == 0) 
+			return true; 
+		return false; 
+            
+        }
+    }
+}
