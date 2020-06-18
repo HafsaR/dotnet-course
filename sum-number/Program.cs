5a6ef6431a47f95fdd62dbba731d12ea5189e1d4
HafsaR <hafsa34343@gmail.com>
2020-06-18 14:45:53 -0600

    "weaves" two strings of equal length with dashes between each letter

---
+++ sum-number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace sum_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*/5. Write a function that takes in two numbers, and returns the sum of all the numbers between them.
+            Example: input is 7 and 12. Output would be 7 + 8 + 9 + 10 + 11 + 12 = 57*/
+            Console.WriteLine("Enter first number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int result = sum_number(number1,number2);
+            Console.WriteLine($"Rsult: {result}");
+
+        }
+        static int sum_number(int n1,int n2){
+            int sum=0;
+            for(int i=n1;i<=n2;i++){
+                sum +=i;
+
+            }
+            return sum;
+        }
+    }
+}
---
+++ weaves-string/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace weaves_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*6. Write a function that "weaves" two strings of equal length with dashes between each letter. Make one string uppercase and one lowercase.
+            Example: Input is "Hello" and "World". Result should be "H-w-E-o-L-r-L-l-O-d"*/
+
+            Console.WriteLine("Enter first string: ");
+            string string1 = Console.ReadLine();
+            Console.WriteLine("Enter second string: ");
+            string string2 = Console.ReadLine();
+            if(string1.Length == string2.Length){
+                string rsult = weaves_string(string1,string2);
+                Console.WriteLine(rsult);
+
+            }else{
+                Console.WriteLine("Input strings are not equal.");
+            }
+        }
+        static string weaves_string(string s1, string s2){
+            string uppercases1 = s1.ToUpper();
+            string lowercases2 = s2.ToLower();
+            string weaves = "";
+            for(int i=0;i<s1.Length;i++){
+                string s1Char = uppercases1[i].ToString();
+                string s2Char = lowercases2[i].ToString();
+                if(i==s1.Length-1){ weaves += string.Join("-",s1Char,s2Char);}
+                else{ weaves += string.Join("-",s1Char,s2Char,"");}
+               
+               // Console.WriteLine($"weaves-->{weaves}");
+
+               // Console.WriteLine($"{uppercases1[i]} {uppercases1.GetType()}");
+
+            }
+            
+            return weaves;
+        }
+    }
+}
